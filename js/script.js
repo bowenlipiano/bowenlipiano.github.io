fb21be776f28f8424b7783536a8764866bbdbca1
Kevin G Lam <kevlam10@outlook.com>
2021-10-17 17:07:52 -0400

    Entry

---
+++ js/script.js
@@ -0,0 +1,103 @@
+/*****************
+
+Kevin G Lam Portfolio
+Kevin G Lam
+js Script for website
+
+******************/
+"use strict";
+
+$(document).ready(setup);
+
+
+
+function setup() {
+
+
+    $("#about").hover(function(){
+  $(this).css("color", "#810FF5");
+  }, function(){
+  $(this).css("color", "black");
+});
+
+$("#work").hover(function(){
+$(this).css("color", "#810FF5");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#cv").hover(function(){
+$(this).css("color", "#810FF5");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#about2").hover(function(){
+$(this).css("color", "#810FF5");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#work2").hover(function(){
+$(this).css("color", "#810FF5");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#cv2").hover(function(){
+$(this).css("color", "#810FF5");
+}, function(){
+$(this).css("color", "black");
+});
+
+$("#kalifront").hover(function(){
+$(this).attr("src", "assets/images/clickme_y.jpg");
+}, function(){
+$(this).attr("src", "assets/images/front/kalifront.jpg");
+});
+
+$("#helenfront").hover(function(){
+$(this).attr("src", "assets/images/clickme_y.jpg");
+}, function(){
+$(this).attr("src", "assets/images/frontjomo.jpg");
+});
+
+$("#mountfront").hover(function(){
+$(this).attr("src", "assets/images/clickme_y.jpg");
+}, function(){
+$(this).attr("src", "assets/images/front/interludefront.jpg");
+});
+
+$("#christyfront").hover(function(){
+$(this).attr("src", "assets/images/clickme_y.jpg");
+}, function(){
+$(this).attr("src", "assets/images/front/chimerafront.jpg");
+});
+
+$("#hubfront").hover(function(){
+$(this).attr("src", "assets/images/clickme_y.jpg");
+}, function(){
+$(this).attr("src", "assets/images/front/paletticafront2.jpg");
+});
+
+$("#dcartfront").hover(function(){
+$(this).attr("src", "assets/images/clickme_y.jpg");
+}, function(){
+$(this).attr("src", "assets/images/grayfront.jpg");
+});
+
+$("#downloadcv").hover(function(){
+$(this).attr("src", "assets/images/downloadcv2.svg");
+}, function(){
+$(this).attr("src", "assets/images/downloadcv.svg");
+});
+
+
+$("#behan").hover(function(){
+$(this).attr("src", "assets/images/behance2.svg");
+}, function(){
+$(this).attr("src", "assets/images/behance.svg");
+});
+
+
+}
